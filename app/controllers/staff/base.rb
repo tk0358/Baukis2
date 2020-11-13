30a3b51f7cfef70af8a689204e90fadbaf5a1712
tk0358 <tk0358a@yahoo.co.jp>
2020-11-13 13:29:35 +0000

    implement Administrator model & implement login, logout routing for admin & admin loginl,ogout link

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,12 +1,9 @@
 class Staff::Base < ApplicationController
-  helper_method :current_staff_member
-
-  private 
-  
-    def current_staff_member
-      if session[:staff_member_id]
-        @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
-      end
+  private def current_staff_member
+    if session[:staff_member_id]
+      @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
     end
+  end
 
+  helper_method :current_staff_member
 end
