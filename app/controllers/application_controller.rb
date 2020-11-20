b962301244dd886fb7dc9cfe424c2595d6ed3d4d
tk0358 <tk0358a@yahoo.co.jp>
2020-11-20 13:13:54 +0000

    implement error page for ParameterMissing(400 Bad Request)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  include ErrorHandlers if Rails.env.production?  
+  include ErrorHandlers if Rails.env.production?
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)})
