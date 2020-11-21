6f07c7456188c5e406165aef627ba35fed8f9177
tk0358 <tk0358a@yahoo.co.jp>
2020-11-21 14:14:24 +0000

    staff, admin向けにアクセス制御authorizeの実装

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,6 @@
-class Admin::Base < ApplicationController  
+class Admin::Base < ApplicationController
+  before_action :authorize
+
   private def current_administrator
     if session[:administrator_id]
       @current_administrator ||= Administrator.find_by(id: session[:administrator_id])
@@ -6,4 +8,11 @@ class Admin::Base < ApplicationController
   end
 
   helper_method :current_administrator
+
+  private def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
+    end
+  end
 end
