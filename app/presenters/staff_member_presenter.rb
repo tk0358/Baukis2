9bbe654a5aed31cebe7ff1f77fa172c5bb1d75b8
tk0358 <tk0358a@yahoo.co.jp>
2020-12-07 21:56:16 +0900

    Add presenter

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,10 +1,18 @@
 class StaffMemberPresenter < ModelPresenter
-  delegate :suspended?, to: :object
+  delegate :suspended?, :family_name, :given_name, :family_name_kana, :given_name_kana,  to: :object
 
   # 職員の停止フラグのOn/Offを表現する記号を返す。
   # On: BALLOT BOX WITH CHECK (U+2611)
   # Off: BALLOT BOX (U+2610)
   def suspended_mark
     suspended? ? raw("&#X2611;") : raw("&#x2610;")
+  end
+
+  def full_name
+    "#{family_name} #{given_name}"
+  end
+
+  def full_name_kana
+    "#{family_name_kana} #{given_name_kana}"
   end
 end
