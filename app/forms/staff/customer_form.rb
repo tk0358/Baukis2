132dc3e4429ee535af635d47b0e75f3c03e7db7f
tk0358 <tk0358a@yahoo.co.jp>
2020-12-14 07:14:20 +0000

    implement create&update phones at customer & home_address

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -14,6 +14,9 @@ class Staff::CustomerForm
     self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
+    (2 - @customer.home_address.phones.size).times do
+      @customer.home_address.phones.build
+    end
   end
 
   def assign_attributes(params = {})
@@ -23,9 +26,29 @@ class Staff::CustomerForm
 
 
     customer.assign_attributes(customer_params)
+    phones = phone_params(:customer).fetch(:phones)
+
+    customer.personal_phones.size.times do |index|
+      attributes = phones[index.to_s]
+      if attributes && attributes[:number].present?
+        customer.personal_phones[index].assign_attributes(attributes)
+      else
+        customer.personal_phones[index].mark_for_destruction
+      end
+    end
 
     if inputs_home_address
       customer.home_address.assign_attributes(home_address_params)
+      phones = phone_params(:home_address).fetch(:phones)
+
+      customer.home_address.phones.size.times do |index|
+        attributes = phones[index.to_s]
+        if attributes && attributes[:number].present?
+          customer.home_address.phones[index].assign_attributes(attributes)
+        else
+          customer.home_address.phones[index].mark_for_destruction
+        end
+      end
     else
       customer.home_address.mark_for_destruction
     end
@@ -42,14 +65,18 @@ class Staff::CustomerForm
   end
 
   private def customer_params
-    @params.require(:customer).permit(:email, :password, :family_name, :given_name, :family_name_kana, :given_name_kana, :birthday, :gender)
+    @params.require(:customer).except(:phones).permit(:email, :password, :family_name, :given_name, :family_name_kana, :given_name_kana, :birthday, :gender)
   end
 
   private def home_address_params
-    @params.require(:home_address).permit(:postal_code, :prefecture, :city, :address1, :address2)
+    @params.require(:home_address).except(:phones).permit(:postal_code, :prefecture, :city, :address1, :address2)
   end
 
   private def work_address_params
     @params.require(:work_address).permit(:postal_code, :prefecture, :city, :address1, :address2, :company_name, :division_name)
+  end
+
+  private def phone_params(record_name)
+    @params.require(record_name).slice(:phones).permit(phones: [ :number, :primary ])
   end
 end
