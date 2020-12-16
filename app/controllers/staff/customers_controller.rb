28a5b4d8ecc4c2b739e4a9133caa4f29a09782ec
tk0358 <tk0358a@yahoo.co.jp>
2020-12-16 09:11:15 +0000

    Implement search function for staff/customer#index

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -1,6 +1,13 @@
 class Staff::CustomersController < Staff::Base
   def index
-    @customers = Customer.order(:family_name_kana, :given_name_kana).page(params[:page])
+    @search_form = Staff::CustomerSearchForm.new(search_params)
+    # @customers = Customer.order(:family_name_kana, :given_name_kana).page(params[:page])
+    @customers = @search_form.search.page(params[:page])
+  end
+
+  private def search_params
+    params[:search]&.permit(:family_name_kana, :given_name_kana,
+      :birth_year, :birth_month, :birth_mday, :address_type, :prefecture, :city, :phone_number)
   end
 
   def show
