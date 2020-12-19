4810bc7bb98f6d5eaa02394d896b5a0c915e8090
tk0358 <tk0358a@yahoo.co.jp>
2020-12-19 06:52:36 +0000

    Implement restrict_ip_address from views

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
         resources :staff_events, only: [ :index ]
       end
       resources :staff_events, only: [ :index ]
+      resources :allowed_sources, only: [:index, :create] do
+        delete :delete, on: :collection
+      end
     end
   end
 
