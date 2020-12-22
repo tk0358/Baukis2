5a4c4694b79df60c4276e4b3b8c14a781f23b11a
tk0358 <tk0358a@yahoo.co.jp>
2020-12-22 05:06:05 +0000

    Add seed data to program & entry

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,5 @@
 class Program < ApplicationRecord
   has_many :entries, dependent: :destroy
-  has_many :applicants, through: entries, source: :customer
+  has_many :applicants, through: :entries, source: :customer
   belongs_to :registrant, class_name: "StaffMember"
 end
