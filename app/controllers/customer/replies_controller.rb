915f02b20fbfe83f34f203b144df27eb85ed562c
tk0358 <tk0358a@yahoo.co.jp>
2021-01-13 06:37:52 +0000

    Implement staff/replies#new,confirm,create

---
+++ app/controllers/customer/replies_controller.rb
@@ -0,0 +1,42 @@
+class Customer::RepliesController < Customer::Base
+  before_action :set_message
+
+  def new
+    @reply = CustomerMessage.new
+  end
+
+  def confirm
+    @reply = CustomerMessage.new(customer_message_params)
+    @reply.parent = @message
+    if @reply.valid?
+      render action: "confirm"
+    else
+      flash.notice.now = "入力に誤りがあります。"
+      render action: "new"
+    end
+  end
+
+  def create
+    @reply = CustomerMessage.new(customer_message_params)
+    if params[:commit]
+      @reply.parent = @message
+      if @reply.save
+        flash.notice = "メッセージに回答しました。"
+        redirect_to :customer_messages
+      else
+        flash.now.alert = "入力に誤りがあります。"
+        render action: "new"
+      end
+    else
+      render action: "new"
+    end
+  end
+
+  private def set_message
+    @message = StaffMessage.find(params[:message_id])
+  end
+
+  private def customer_message_params
+    params.require(:customer_message).permit(:subject, :body)
+  end
+end
